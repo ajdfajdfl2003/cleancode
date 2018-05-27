86c3b62749ce23a830874bc89c2190d56296145a
AngusWu <anguswu@simbo.com.tw>
2018-05-27 23:35:38 +0800

    Refactor: remove comment and exchange if-else order

--- src/main/java/GeneratePrimes.java
+++ src/main/java/GeneratePrimes.java
@@ -1,28 +1,20 @@
 /**
- * This class Generates prime numbers up to a user specified maximum. The
- * algorithm used is the Sieve of Eratosthenes.
- * <p>
- * Eratosthenes of Cyrene, b. c. 276 BC, Cyrene, Libya -- d. c. 194, Alexandria.
- * The first man to calculate the circumference of the Earth. Also known for
- * working on calendars with leap years and ran the library at Alexandria.
- * <p>
- * The algorithm is quite simple. Given an array of integers starting at 2.
- * Cross out all multiples of 2. Find the next uncrossed integer, and cross out
- * all of its multiples. Repeat untilyou have passed the square root of the
- * maximum value.
- *
- * @author Alphonse
- * @version 13 Feb 2002 atp
+ * This class Generates prime numbers up to a user specified maximum.
+ * The algorithm used is the Sieve of Eratosthenes.
+ * Given an array of integers starting at 2.
+ * Find the first uncrossed integer, and cross out
+ * all of its multiples.
+ * Repeat until there are no more multiples in the array.
  */
 
 public class GeneratePrimes {
-    /**
-     * @param maxValue
-     *            is the generation limit.
-     */
+
     public static int[] generatePrimes(int maxValue) {
-        if (maxValue >= 2) // the only valid case
-        {
+        if (maxValue < 2) {
+
+            return new int[0];
+
+        } else {
             // declarations
             int s = maxValue + 1; // size of array
             boolean[] f = new boolean[s];
@@ -54,8 +46,6 @@ public class GeneratePrimes {
                     primes[j++] = i;
             }
             return primes; // return the primes
-        } else
-            // maxValue < 2
-            return new int[0]; // return null array if bad input.
+        }
     }
 }
